1fbda7c3aff7a7e614fbb77dbe390f00bea8fb4e
Wojciech Owczarczyk <voodoorider@gmail.com>
2018-08-20 08:16:51 +0200

    Update server.js
    Test change

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/pagecount', function (req, res) {
   }
   if (db) {
     db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
+      res.send('{ pageCount2: ' + count + '}');
     });
   } else {
     res.send('{ pageCount: -1 }');
